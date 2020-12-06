a2386a514e2d98e621ccead0f2ae000e382c41bd
Guvanch Garryyev <49387249+guvanchgarryyev@users.noreply.github.com>
2020-12-06 14:12:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 /**
  * Weather App
- * Done: Complete getWeatherData() to return json response Promise
- * Done: Complete searchCity() to get user input and get data using getWeatherData()
- * TODO: Complete showWeatherData() to set the data in the the html file from response
  */
 
 // API_KEY for maps api
@@ -10,15 +7,15 @@ let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
 
 /**
  * Retrieve weather data from openweathermap
- * HINT: Use fetch()
- * HINT: URL should look like this: 
+ * Use fetch()
+ * URL should look like this: 
  * https://api.openweathermap.org/data/2.5/weather?q=detroit&appid=a8e71c9932b20c4ceb0aed183e6a83bb&units=imperial
  */
 getWeatherData = (city) => {
   const URL = "https://api.openweathermap.org/data/2.5/weather";
   //HINT: Use template literals to create a url with input and an API key
   const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=imperial`;
-  //CODE GOES HERE
+  
   const weatherLink = fetch(FULL_URL);
   return weatherLink.then((response)=> {
     return response.json();
@@ -27,11 +24,11 @@ getWeatherData = (city) => {
 
 /**
  * Retrieve city input and get the weather data
- * HINT: Use the promise returned from getWeatherData()
+ * Use the promise returned from getWeatherData()
  */
 searchCity = () => {
   const city = document.getElementById('city-input').value;
-  // CODE GOES HERE
+  
   getWeatherData(city)
   .then((response)=>{
     showWeatherData(response);
@@ -43,10 +40,8 @@ searchCity = () => {
 
 /**
  * Show the weather data in HTML
- * HINT: make sure to console log the weatherData to see how the data looks like
  */
 showWeatherData = (weatherData) => {
-  //CODE GOES HERE
   document.getElementById("city-name").innerText = weatherData.name;
   document.getElementById("weather-type").innerText = weatherData.weather[0].main;
   document.getElementById("temp").innerText = weatherData.main.temp;
